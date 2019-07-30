b9998f85b8702a3da11369f520c22777aee141d8
kbutkus@gmail.com <kbutkus@gmail.com>
2019-07-30 04:24:05 +0000

    lessons

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :instructor do
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create] 
     end
