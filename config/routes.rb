f52e6c9aa569142876b2bfb4cb638184b4329278
kbutkus@gmail.com <kbutkus@gmail.com>
2019-07-30 03:18:33 +0000

    allow course creation

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   #devise_for :models
   #devise_for :installs
   root 'static_pages#index'
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
   end
+end
