e715bdb368bc0ef8e7bf52b8a8960c3e024e78f8
kbutkus@gmail.com <kbutkus@gmail.com>
2019-07-30 03:33:53 +0000

    listing all the courses

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   #devise_for :models
   #devise_for :installs
   root 'static_pages#index'
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end
