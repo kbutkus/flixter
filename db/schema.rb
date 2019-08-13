3ee46373d5f304dcccdd11c4d611b77625db5f42
kbutkus@gmail.com <kbutkus@gmail.com>
2019-08-13 02:14:00 +0000

    ordering lessons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_044128) do
+ActiveRecord::Schema.define(version: 2019_08_13_020729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2019_08_08_044128) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "video"
+    t.integer "row_order"
+    t.index ["row_order"], name: "index_lessons_on_row_order"
     t.index ["section_id"], name: "index_lessons_on_section_id"
   end
 
