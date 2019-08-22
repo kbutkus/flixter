98d4e8fa854ec22a8e2c9cd013810caa121eeb37
kbutkus@gmail.com <kbutkus@gmail.com>
2019-08-22 02:17:40 +0000

    fixing bugs

--- db/migrate/20190808004638_add_image_to_courses.rb
+++ db/migrate/20190808004638_add_image_to_courses.rb
@@ -1,11 +1,6 @@
 class AddImageToCourses < ActiveRecord::Migration[5.2]
   def change
-    create_table :enrollments do |t|
-      t.integer :user_id
-      t.integer :course_id
-      t.timestamps
-    end
-    add_index :enrollments, [:user_id, :course_id]
-    add_index :enrollments, :course_id
+    
+    add_column :courses, :image, :string
   end
 end
